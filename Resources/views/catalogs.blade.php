da0ea2f9be4e3f03d6e953946514480f36f25e5e
h.chobanov <hristiqn.chobanov@gmail.com>
2024-06-06 09:20:36 +0300

    change

--- Resources/views/catalogs.blade.php
+++ Resources/views/catalogs.blade.php
@@ -1,14 +1,16 @@
 @if(!is_null($catalogs) && $catalogs->isNotEmpty())
-    <ul class="list-thumbnails">
-        @foreach($catalogs as $catalog)
-            <li data-aos="fade-up">
-                <div class="thumbnail-image">
-                    <a href="{{ route('front.catalogs.index', ['languageSlug' => $languageSlug, 'catalogId' => $catalog->main_catalog_id]) }}" target="_blank">
-                        <img src="{{ $catalog->parent->translate($languageSlug)->fullImageFilePathUrl() }}" alt="{{ $catalog->short_description }}">
-                    </a>
-                </div>
-                <a href="{{ route('front.catalogs.index', ['languageSlug' => $languageSlug, 'catalogId' => $catalog->main_catalog_id]) }}" target="_blank">{{ $catalog->short_description }}</a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="list-thumbnails">
+        <ul>
+            @foreach($catalogs as $catalog)
+                <li data-aos="fade-up">
+                    <div class="thumbnail-image">
+                        <a href="{{ route('front.catalogs.index', ['languageSlug' => $languageSlug, 'catalogId' => $catalog->main_catalog_id]) }}" target="_blank">
+                            <img src="{{ $catalog->parent->translate($languageSlug)->fullImageFilePathUrl() }}" alt="{{ $catalog->short_description }}">
+                        </a>
+                    </div>
+                    <a href="{{ route('front.catalogs.index', ['languageSlug' => $languageSlug, 'catalogId' => $catalog->main_catalog_id]) }}" target="_blank">{{ $catalog->short_description }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endif
